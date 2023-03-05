a6a4f952daf7ed71c108d01e353f9641c74a56da
Rajat <rajatprajapati09@gmail.com>
2023-03-05 23:58:48 +0530

    Employee_Payroll program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Payroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome Employee Paroll");
+        }
+    }
+}
